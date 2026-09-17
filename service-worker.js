ba968ec0833257316d5b4e6791bac84606a62039
gouijj <moreaujulien0@gmail.com>
2026-09-17 15:53:03 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'jahnael-v12';
+const CACHE_NAME = 'jahnael-v13';
 const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', function (event) {
